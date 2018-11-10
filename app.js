9a1114dfeda7cd9093726ced6dd52b99cce86269
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-11-10 23:14:46 +1100

    Update app to a function and throw errors

--- app.js
+++ app.js
@@ -1,45 +1,49 @@
 const fs = require('fs')
 
-// Capture markdown file
-const matchMarkdownFile = /[0-9A-Za-z]+.md/ig
-const fileArgument = process.argv.filter(arguments => arguments.startsWith('--file=') && arguments.match(matchMarkdownFile) && arguments.length > 10)[0]
-const file = !!fileArgument && fileArgument.replace('--file=','')
+const app = () => {
+  // Capture markdown file
+  const matchMarkdownFile = /[0-9A-Za-z]+.md/ig
+  const fileArgument = process.argv.filter(arguments => arguments.startsWith('--file=') && arguments.match(matchMarkdownFile) && arguments.length > 10)[0]
+  const file = !!fileArgument && fileArgument.replace('--file=','')
 
-// Capture heading level
-const headingLevelArgument = process.argv.filter(arguments => arguments.startsWith('--headingLevel=') && arguments.length > 15)[0]
-const intHeadingLevel = parseInt(headingLevelArgument.replace('--headingLevel=',''))
+  // Capture heading level
+  const headingLevelArgument = process.argv.filter(arguments => arguments.startsWith('--headingLevel=') && arguments.length > 15)[0]
+  const intHeadingLevel = parseInt(headingLevelArgument.replace('--headingLevel=',''))
 
-// Heading level is by default 6 if not specified correctly
-const headingLevel = (!!headingLevelArgument && intHeadingLevel>0 && intHeadingLevel<7 && intHeadingLevel) || 6
+  // Heading level is by default 6 if not specified correctly
+  const headingLevel = (!!headingLevelArgument && intHeadingLevel>0 && intHeadingLevel<7 && intHeadingLevel) || 6
 
-if(!file || !file.endsWith('.md')) {
-  console.error('Markdown file is required.')
-} else {
-  try {
-    const contents = fs.readFileSync(file,'utf8')
-    //console.log (contents)
+  if(!file || !file.endsWith('.md')) {
+    throw new Error('Markdown file is required.')
+  } else {
+    try {
+      const contents = fs.readFileSync(file,'utf8')
+      //console.log (contents)
 
-    const matchHeading = new RegExp(`^#{1,${headingLevel}}\\s`)
-    const matchSpaces = /\s+/g
+      const matchHeading = new RegExp(`^#{1,${headingLevel}}\\s`)
+      const matchSpaces = /\s+/g
 
-    console.log('## Table of Contents')
-    contents.split('\n').map( (line, i) => {
-      // if(line.startsWith('# ')
-      //     || line.startsWith('## ')
-      //     || line.startsWith('### ')
-      //     || line.startsWith('#### ')
-      //     || line.startsWith('##### ')
-      //     || line.startsWith('###### ')) {
-      if(line.match(matchHeading) && !line.startsWith('## Table of Contents')) {
-          
-        // console.log(line.toLowerCase())
-        const title = line.replace(matchHeading, '')
-        const anchorLink = '(#' + title.toLowerCase().replace(matchSpaces, '-')+')'
-        console.log('* **['+title+']'+anchorLink+'**')
-      }
-    })
-  }
-  catch(error) {
-    console.error('Error reading file.')
+      console.log('## Table of Contents')
+      contents.split('\n').map( (line, i) => {
+        // if(line.startsWith('# ')
+        //     || line.startsWith('## ')
+        //     || line.startsWith('### ')
+        //     || line.startsWith('#### ')
+        //     || line.startsWith('##### ')
+        //     || line.startsWith('###### ')) {
+        if(line.match(matchHeading) && !line.startsWith('## Table of Contents')) {
+            
+          // console.log(line.toLowerCase())
+          const title = line.replace(matchHeading, '')
+          const anchorLink = '(#' + title.toLowerCase().replace(matchSpaces, '-')+')'
+          console.log(`* **[${title}]${anchorLink}**`)
+        }
+      })
+    }
+    catch(error) {
+      throw new Error('Error reading file.')
+    }
   }
 }
+
+module.exports = { app }
