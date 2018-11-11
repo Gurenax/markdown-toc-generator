fd4752f54473d18596d38f2f4b39325c1c5a99fb
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-11-11 17:58:06 +1100

    Update intHeadingLevel validation

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const getFileFromArgs = args => {
 const getHeadingLevelFromArgs = args => {
   // Capture heading level
   const headingLevelArgument = args.filter(arg => arg.startsWith('--headingLevel=') && arg.length > 15)[0]
-  const intHeadingLevel = parseInt(headingLevelArgument.replace('--headingLevel=',''))
+  const intHeadingLevel = !!headingLevelArgument && parseInt(headingLevelArgument.replace('--headingLevel=',''))
 
   // Heading level is by default 6 if not specified correctly
   return (!!headingLevelArgument && intHeadingLevel>0 && intHeadingLevel<7 && intHeadingLevel) || 6
