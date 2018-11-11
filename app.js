78b57b429a51c7716a4c33e9dda4e8554535ae8c
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-11-11 17:52:25 +1100

    Update app into modular functions

--- app.js
+++ app.js
@@ -1,49 +1,53 @@
 const fs = require('fs')
 
-const app = () => {
-  // Capture markdown file
-  const matchMarkdownFile = /[0-9A-Za-z]+.md/ig
-  const fileArgument = process.argv.filter(arguments => arguments.startsWith('--file=') && arguments.match(matchMarkdownFile) && arguments.length > 10)[0]
-  const file = !!fileArgument && fileArgument.replace('--file=','')
-
-  // Capture heading level
-  const headingLevelArgument = process.argv.filter(arguments => arguments.startsWith('--headingLevel=') && arguments.length > 15)[0]
-  const intHeadingLevel = parseInt(headingLevelArgument.replace('--headingLevel=',''))
-
-  // Heading level is by default 6 if not specified correctly
-  const headingLevel = (!!headingLevelArgument && intHeadingLevel>0 && intHeadingLevel<7 && intHeadingLevel) || 6
-
-  if(!file || !file.endsWith('.md')) {
+const createTableOfContents = (file, headingLevel=6) => {
+  let output = ''
+  if(!file) {
     throw new Error('Markdown file is required.')
   } else {
     try {
       const contents = fs.readFileSync(file,'utf8')
-      //console.log (contents)
-
       const matchHeading = new RegExp(`^#{1,${headingLevel}}\\s`)
       const matchSpaces = /\s+/g
 
-      console.log('## Table of Contents')
-      contents.split('\n').map( (line, i) => {
-        // if(line.startsWith('# ')
-        //     || line.startsWith('## ')
-        //     || line.startsWith('### ')
-        //     || line.startsWith('#### ')
-        //     || line.startsWith('##### ')
-        //     || line.startsWith('###### ')) {
+      output += '## Table of Contents\n'
+      contents.split('\n').map( line => {
         if(line.match(matchHeading) && !line.startsWith('## Table of Contents')) {
-            
-          // console.log(line.toLowerCase())
           const title = line.replace(matchHeading, '')
           const anchorLink = '(#' + title.toLowerCase().replace(matchSpaces, '-')+')'
-          console.log(`* **[${title}]${anchorLink}**`)
+          output += `* **[${title}]${anchorLink}**\n`
         }
       })
     }
     catch(error) {
       throw new Error('Error reading file.')
     }
+
+    return output
   }
 }
 
-module.exports = { app }
+const getFileFromArgs = args => {
+  const matchMarkdownFile = /[0-9A-Za-z]+.md/ig
+  const fileArgument = args.filter(arg => arg.startsWith('--file=') && arg.match(matchMarkdownFile) && arg.length > 10)[0]
+  return !!fileArgument && fileArgument.replace('--file=','')
+}
+
+const getHeadingLevelFromArgs = args => {
+  // Capture heading level
+  const headingLevelArgument = args.filter(arg => arg.startsWith('--headingLevel=') && arg.length > 15)[0]
+  const intHeadingLevel = parseInt(headingLevelArgument.replace('--headingLevel=',''))
+
+  // Heading level is by default 6 if not specified correctly
+  return (!!headingLevelArgument && intHeadingLevel>0 && intHeadingLevel<7 && intHeadingLevel) || 6
+}
+
+const app = () => {
+  // Capture markdown file
+  const file = getFileFromArgs(process.argv)
+  const headingLevel = getHeadingLevelFromArgs(process.argv)
+  const output = createTableOfContents(file, headingLevel)
+  console.log(output)
+}
+
+module.exports = { createTableOfContents, app }
