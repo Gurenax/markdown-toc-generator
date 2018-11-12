4f23e8d370aa876a5a07fde1b78782f387d51b51
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-11-12 22:45:53 +1100

    Refactor getHeadingLevelFromArgs

--- src/getHeadingLevelFromArgs.js
+++ src/getHeadingLevelFromArgs.js
@@ -1,10 +1,28 @@
+/**
+ * @description Get the headingLevel from the --headingLevel runtime argument
+ * @param {*} args - Array arguments
+ */
 const getHeadingLevelFromArgs = args => {
-  // Capture heading level
-  const headingLevelArgument = args.filter(arg => arg.startsWith('--headingLevel=') && arg.length > 15)[0]
-  const intHeadingLevel = !!headingLevelArgument && parseInt(headingLevelArgument.replace('--headingLevel=',''))
+  // Filter the heading level from the arguments
+  const headingLevelArgument = args.filter(
+    arg => arg.startsWith("--headingLevel=") && arg.length > 15
+  )[0]
+
+  // Parse the heading level value to Integer
+  const intHeadingLevel =
+    !!headingLevelArgument &&
+    parseInt(headingLevelArgument.replace("--headingLevel=", ""))
 
   // Heading level is by default 6 if not specified correctly
-  return (!!headingLevelArgument && intHeadingLevel>0 && intHeadingLevel<7 && intHeadingLevel) || 6
+  return (
+    (!!headingLevelArgument &&
+      intHeadingLevel > 0 &&
+      intHeadingLevel < 7 &&
+      intHeadingLevel) ||
+    6
+  )
 }
 
-export default getHeadingLevelFromArgs
+module.exports = {
+  getHeadingLevelFromArgs
+}
