ee3cd5cef902ee235b08d58dec50ce1d97057d13
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-11-13 20:23:54 +1100

    Refactor app

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const { getFileFromArgs } = require('./src/getFileFromArgs')
 const { getHeadingLevelFromArgs } = require('./src/getHeadingLevelFromArgs')
+const { getContentsFromFile } = require('./src/getContentsFromFile')
 const { createTableOfContents } = require('./src/createTableOfContents')
 
 const app = () => {
   // Capture markdown file
   const file = getFileFromArgs(process.argv)
   const headingLevel = getHeadingLevelFromArgs(process.argv)
-  const output = createTableOfContents(file, headingLevel)
+  if (!file) {
+    throw new Error("Markdown file is required.")
+  }
+  const contents = getContentsFromFile(file)
+  const output = createTableOfContents(contents, headingLevel)
   console.log(output)
 }
 
