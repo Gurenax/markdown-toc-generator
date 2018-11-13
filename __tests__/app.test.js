80d579abe2b3424990a14601401b468879933fb4
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-11-13 20:24:02 +1100

    Refactor app tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,45 @@
-const { createTableOfContents } = require('../app.js')
+const { app } = require("../app")
 
-test('Test createTableOfContents()', () => {
-  const output = createTableOfContents('Example.md', '6')
-  expect(output).toBeTruthy()
-  console.log(output)
+beforeEach(() => {
+  // Add File argument
+  process.argv.push('--file=Example.md')
+  // Add Heading level argument
+  process.argv.push('--headingLevel=6')
+});
+
+test("Test app()", () => {
+  expect(() => app()).not.toThrowError()
 })
+
+test("Test app() - no arguments", () => {
+  // Remove heading level argument
+  process.argv.pop()
+  // Remove file argument
+  process.argv.pop()
+  expect(() => app()).toThrowError()
+})
+
+test("Test app() - no file argument", () => {
+  // Remove heading level argument
+  process.argv.pop()
+  // Remove file argument
+  process.argv.pop()
+  expect(() => app()).toThrowError()
+})
+
+test("Test app() - no heading level argument", () => {
+  // Remove file argument
+  process.argv.pop()
+  // Remove heading level argument
+  process.argv.pop()
+  // Add File argument
+  process.argv.push('--file=Example.md')
+  expect(() => app()).not.toThrowError()
+})
+
+afterEach(() => {
+  // Remove heading level argument
+  process.argv.pop()
+  // Remove file argument
+  process.argv.pop()
+});
