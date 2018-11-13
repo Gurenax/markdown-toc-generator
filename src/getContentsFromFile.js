a959fa2f8e4972e2829e7a31148b4875ed13b575
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-11-13 20:23:22 +1100

    Add getContentsFromFile

---
+++ src/getContentsFromFile.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+
+/**
+ * @description Read file and get the contents
+ * @param {*} file - The file to be read
+ */
+const getContentsFromFile = file => {
+  try {
+    return fs.readFileSync(file, "utf8")
+  }
+  catch(error) {
+    throw new Error("Error reading file. "+error)
+  }
+}
+
+module.exports = {
+  getContentsFromFile
+}
